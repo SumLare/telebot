db347412e3557cc4d3d84545e64065502a255c8c
SumLare <anohin56n@gmail.com>
2017-10-22 21:55:07 +0300

    New cli command

---
+++ lib/telebot/cli/new.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Telebot
+  class CLI::New
+    attr_reader :bot_name
+
+    def initialize(bot_name)
+      @bot_name = bot_name
+    end
+
+    def run
+
+    end
+  end
+end
